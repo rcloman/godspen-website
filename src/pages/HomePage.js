34be469894d5b525ccaf1884a95849d516ada81b
rcloman <jwanbaughn@aol.com>
2025-06-23 01:17:19 -0500

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import authorImage from '../assets/Reginapic2updated.jpg';
-import floridaPeppermintCover from '../assets/florida_peppermint_book_cover.jpeg';
-import grandmasLapCover from '../assets/Grandma\'s Lap.JPG';
+import floridaPeppermintCover from '../assets/florida_peppermint_book_cover.jpeg;
 import fathersVoiceCover from '../assets/I know My Father\'s Voice.jpg';
 
 const HomePage = () => {
